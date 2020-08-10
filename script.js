bfa3cf2584b6aa8054053cba3077419d75b21fde
Josiel Junior <juniorfoxjbs@gmail.com>
2020-08-10 20:28:23 -0300

    Exibindo a janela para add as pizzas que serão compradas. Criando opacidade em 0,5 segundos.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const c = (el)=> document.querySelector(el); //Retorna o item.
-const cs = (el)=> document.querySelectorAll(el)//Retorna uma array com os itens que achou.
+const c = (el)=> { return document.querySelector(el);} //Retorna o item.
+
+/*const c = (el)=> document.querySelector(el); //A mesma coisa do de cima.*/
+
+const cs = (el)=> document.querySelectorAll(el)//Retorna uma array com os itens que achou.""
 
 pizzaJson.map((item, index)=>{
   let pizzaitem = c('.models .pizza-item').cloneNode(true)//Clona para a variável o que está na div 'pizza-item.
@@ -11,9 +14,15 @@ pizzaJson.map((item, index)=>{
   pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//tofixed(2) define duas casas depois da vígula.
   pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
   pizzaitem.querySelector('a').addEventListener('click', (e)=>{
     e.preventDefault();//Previne contra evento padrão.
-    console.log('clicou');
+
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex';//Torna visível ao usuário. Pois está 'none' de padrão.
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1;//Após 200 milisegundos ele mostra 'opacity 1'.
+    }, 200);//Tempo em milisegundos.
   })
 
   c('.pizza-area').append(pizzaitem);//Adiciona 'pizzaitem' no html de ' pizza-area'
