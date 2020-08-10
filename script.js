ec900cab024800dc9e1b54a6ba45fb067ed92069
Josiel Junior <juniorfoxjbs@gmail.com>
2020-08-10 19:42:31 -0300

    Entedendo o projeto e exibindo as informações das pizza no tela

---
+++ script.js
@@ -0,0 +1,17 @@
+const c = (el)=> document.querySelector(el); //Retorna o item.
+const cs = (el)=> document.querySelectorAll(el)//Retorna uma array com os itens que achou.
+
+pizzaJson.map((item, index)=>{
+  let pizzaitem = c('.models .pizza-item').cloneNode(true)//Clona para a variável o que está na div 'pizza-item.
+
+  //Não importa a ordem que colocarmos aqui, ele exibe do jeito que está no html.
+
+  
+  pizzaitem.querySelector('.pizza-item--img img').src = item.img;//Substitui no 'scr' e não mais no innerHMTL
+  pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//tofixed(2) define duas casas depois da vígula.
+  pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  c('.pizza-area').append(pizzaitem);//Adiciona 'pizzaitem' no html de ' pizza-area'
+
+});
