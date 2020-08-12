55629d8efe5a50b1f6a6937cf6ce94219390692c
Josiel Junior <juniorfoxjbs@gmail.com>
2020-08-11 21:11:51 -0300

    1° parte da criação de eventos para o modal. Criando ação de fechar ao clicar no butão cancelar.

--- script.js
+++ script.js
@@ -4,26 +4,28 @@ const c = (el)=> { return document.querySelector(el);} //Retorna o item.
 
 /*const c = (el)=> document.querySelector(el); //A mesma coisa do de cima.*/
 
-const cs = (el)=> document.querySelectorAll(el)//Retorna uma array com os itens que achou.""
+const cs = (el)=> document.querySelectorAll(el)//Retorna uma array com os itens que achou. Usado para executar em mais de um item.
 
+//ESTAMOS MAPEANDO AS DIFERENTES PIZZAS QUE ESTÃO NO PIZZAJSON
+//DENTRO DESSE BLOCO DE CÓDIGO VAI TUDO SOBRE AS INFORMAÇÕES DAS PIZZAS TANTO NA TELA DE ESCOLHA DE SABOR, QUANDO NA ESCOLHA DE QUANTIDADES
 pizzaJson.map((item, index)=>{
   let pizzaitem = c('.models .pizza-item').cloneNode(true)//Clona para a variável o que está na div 'pizza-item.
 
   //Não importa a ordem que colocarmos aqui, ele exibe do jeito que está no html.
-
   pizzaitem.setAttribute('data-key', index);//Adicionando um atributo para cada pizza. O index chamado 'data-key'.
   pizzaitem.querySelector('.pizza-item--img img').src = item.img;//Substitui no 'scr' e não mais no innerHMTL
   pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;//tofixed(2) define duas casas depois da vígula.
   pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
+  //EVENTO DE CLICK PARA ABRIR O MODAL
   pizzaitem.querySelector('a').addEventListener('click', (e)=>{//São todos os eventos que ocorrerão ao clicar no imagem da pizza.
     e.preventDefault();//Previne contra evento padrão.
     
     //Criando uma varável que vai receber o index de cada pizza.
     //Estamos buscando um elemento anterior o 'e' ou 'a' ao usar o target. Depois pegamos o atributo 'data-key' com o getAttribute.
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
-    modalqt = 1;
+    modalqt = 1;//Sempre quando abrir o modal de ecolha de pizzas, ele reseta a quantidade de pizza para 1.
 
     c('.pizzaBig img').src = pizzaJson[key].img;
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -39,10 +41,7 @@ pizzaJson.map((item, index)=>{
       size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeindex];//Está substituindo o 'span' pelo tamnho da pizza. No caso sizes[sizeindex].
     });
 
-    c('.pizzaInfo--qt').innerHTML = modalqt;
-
-
-
+    c('.pizzaInfo--qt').innerHTML = modalqt;//Substituindo a quantidade de pizza por uma variável.
 
     c('.pizzaWindowArea').style.opacity = 0;//Transparente.
     c('.pizzaWindowArea').style.display = 'flex';//Torna visível ao usuário. Pois está 'none' de padrão.
@@ -53,4 +52,17 @@ pizzaJson.map((item, index)=>{
 
   c('.pizza-area').append(pizzaitem);//Adiciona 'pizzaitem' no html de ' pizza-area'
 
+});
+
+//EVENTOS DO MODAL
+
+function closemodal (){//Função para sumir o modal da tela.
+  c('.pizzaWindowArea').style.opacity = 0;//Para sumir tem que ter opacity 0. Ele fica tranparente. 
+  setTimeout(() => {
+    c('.pizzaWindowArea').style.display = 'none';//Depois de meio segundo ele some de vez da tela.
+  }, 500);//Meio segundo.
+};
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{//Ele executa nos dois elementos.
+  item.addEventListener('click', closemodal);//Ao clicar nos elementos ele executa a função 'closemodal'
 });
